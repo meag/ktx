c8f058b5b2431123a167b1752d8b7142c739a87a
meag <meag@acm.org>
2016-11-17 20:03:12 +0000

    ZONES: Changing Z_ to be 1-based rather than 0

--- src/weapons.c
+++ src/weapons.c
@@ -1688,9 +1688,6 @@ float W_BestWeapon()
 	int   it = self->s.v.items;
 	char* w_rank = ezinfokey( self, "w_rank" );
 
-	if (self->isBot)
-		return;
-
 	if ( ! strnull(w_rank) )
 	{
 		while (*w_rank)
